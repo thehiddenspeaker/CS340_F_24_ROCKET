0f3843eb2ba2ecce150061ca407eb0517eb744d4
gamer <gamerslaw2000@gmail.com>
2024-11-12 14:43:46 -0600

    test-trent law

---
+++ module.py
@@ -0,0 +1,11 @@
+import requests
+url = "https://mhw-db.com/weapons/17"
+param = {
+    'fields': 'name,id,type'
+}
+array = requests.get("https://mhw-db.com/weapons")
+array2 = requests.get(url, params=param)
+data = array2.json()
+
+print(data)
+
