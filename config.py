6f6f57af0a9010f863754e49a817196b12b5889e
King-Junco <brennankimbrell0601@gmail.com>
2024-11-19 14:18:06 -0600

    made the config file he wanted

---
+++ config.py
@@ -0,0 +1,24 @@
+#%% MODULE BEGINS
+module_name = 'config'
+
+'''
+Version: 1
+
+Description:
+    config for team rockets project 
+
+Authors:
+    Brennan Kimbrell
+    Trent Law 
+    morgan montet 
+
+Date Created     :  11/19/2024
+Date Last Updated:  11/19/2024
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
