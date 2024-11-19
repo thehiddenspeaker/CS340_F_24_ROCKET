5258d400b1c7798f2d7d31f94f1d802ffa2f19ae
King-Junco <brennankimbrell0601@gmail.com>
2024-11-19 14:23:44 -0600

    made the templet for the parent and class

---
+++ csv_parent_child.py
@@ -0,0 +1,58 @@
+#%% MODULE BEGINS
+module_name = 'csv_parent_child'
+
+'''
+Version: 1
+
+Description:
+    The parent and child class that read and interpret data from a csv file
+
+Authors:
+    Brennan Kimbrell
+    Trent Law 
+    morgan montet 
+
+Date Created     :  11/19/2024
+Date Last Updated:  11/19/2024
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#custom imports
+
+
+#other imports
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+
+
+
+#Function definitions Start Here
+
