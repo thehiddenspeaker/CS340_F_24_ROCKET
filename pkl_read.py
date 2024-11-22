b0f19fdf195e85e0658295158e6caea6b11ef6e5
thehiddenspeaker <gamerslaw2000@gmail.com>
2024-11-21 19:48:28 -0600

    added two pkl class files

---
+++ pkl_read.py
@@ -0,0 +1,69 @@
+#%% MODULE BEGINS
+module_name = 'pkl_read'
+
+'''
+Version: <***>
+
+Description:
+    <***>
+
+Authors:
+    <***>
+
+Date Created     :  <***>
+Date Last Updated:  <***>
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+   import os
+   #os.chdir("./../..")
+#
+
+#custom imports
+
+
+#other imports
+from   copy       import deepcopy as dpcpy
+
+'''
+from   matplotlib import pyplot as plt
+import mne
+import numpy  as np 
+import os
+import pandas as pd
+import seaborn as sns
+'''
+#%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+class pkl_read:
+
+
+
+#Function definitions Start Here
+
+#end of class pkl_read
